fa2b0406c17545a1424eb505743e4ccdda3831d7
Bongani Gumede <Mtesazi@gmail.com>
2026-04-10 00:17:42 +0200

    Rename H2 console test to default profile naming

--- src/test/java/com/pollinate/H2ConsoleDefaultProfileTest.java
+++ src/test/java/com/pollinate/H2ConsoleDefaultProfileTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class H2ConsoleDevProfileTest {
+class H2ConsoleDefaultProfileTest {
 
     @LocalServerPort
     private int port;
