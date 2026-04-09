d2f09037de06001649897b4503febf25110794c2
Bongani Gumede <Mtesazi@gmail.com>
2026-04-09 17:20:01 +0200

    Creation of the whole project.

---
+++ src/main/java/com/pollinate/OrderManagementMicroservices.java
@@ -0,0 +1,11 @@
+package com.pollinate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementMicroservices {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementMicroservices.class, args);
+    }
+}
